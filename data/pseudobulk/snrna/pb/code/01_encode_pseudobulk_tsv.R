288bebd81870e75b62beb249ce9770c199818eaa
whou10 <wp.hou3@gmail.com>
2024-08-14 10:48:35 -0400

    code/01_encode_pseudobulk_tsv.R

--- data/pseudobulk/snrna/pb/code/01_encode_pseudobulk_tsv.R
+++ data/pseudobulk/snrna/pb/code/01_encode_pseudobulk_tsv.R
@@ -3,13 +3,16 @@ source('/home/whou10/scratch4/whou10/encode4/data/pseudobulk/snrna/pb/code/00_ge
 ## load ct annotation file and prepare pseudobulk level123
 ## ========================================================
 library(Matrix)
-d = readRDS('/home/whou10/scratch4/whou10/encode4/data/celltype/proc/final.rds')
-d <- d[!is.na(d$rna_dataset), ]
+# d = readRDS('/home/whou10/scratch4/whou10/encode4/data/celltype/proc/old/final.rds')
+
+d = readRDS('/home/whou10/scratch4/whou10/encode4/data/celltype/proc/step3.rds') ## 	1520779 obs. of  18 variables:
+
+d <- d[!is.na(d$rna_dataset), ] # 	870041 obs. of  18 variables
 d$lifestage <-
   ifelse(d$lifestage %in% c('adult', 'child'),
          'adult_child',
          'embryo_postnatal')
-
+colnames(d) <- tolower(colnames(d))
 l <-
   data.frame(
     barcode = paste0(d$rna_dataset, ':', d$rna_library, ':', d$rna_barcode),
@@ -45,7 +48,7 @@ l <-
       )
     )
   )
-str(l)
+str(l) ## 	870041 obs. of  4 variables:
 saveRDS(l, '/home/whou10/scratch4/whou10/encode4/data/pseudobulk/snrna/pb/merge/levels.rds')
 
 
@@ -63,9 +66,10 @@ for (level in paste0('pseudobulk_level', 1:3)) {
   for (i in alli) {
     gene_quantification_pseudobulk(
       dataset_path = '/home/whou10/data/zji/encode/data/scrna/mat/mat/',
-      dataset = paste0(unique(sub(':.*','',bc)),'.rds'),
+      dataset = paste0(unique(sapply(l[l[,level]==i, 1], function(i){sub(':.*','',i)})),'.rds'),
       barcode = l[l[,level]==i, 1],
       savefilename = paste0(rdir, i, '.tsv')
     )
   }
 }
+
